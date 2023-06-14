f983c335db48d38e5990a2bdb427ab38ad32191a
Sidiki kaba <kabasidiki2020@gmail.com>
2023-06-14 17:03:53 +0200

    modification du input de partage

--- front_end/src/Components/share/Share.js
+++ front_end/src/Components/share/Share.js
@@ -17,62 +17,113 @@ const Share = () => {
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
-  return (
-    <div className="share">
-      <div className="container">
-        <div className="top">
-          <input placeholder={`Quoi de neuf /*{Sidiki Kaba}*/?*`} />
-        </div>
-        <hr />
-        <div className="bottom">
-          <div className="left">
-            <input type="file" id="file" style={{ display: "none" }} />
-            <label htmlFor="file">
-              <div className="item">
-                <img src={Photo} alt="" />
-                <span>Photo</span>
-              </div>
-            </label>
-            <input type="file" id="video" style={{ display: "none" }} />
-            <label htmlFor="file">
-              <div className="item">
-                <img src={Video} alt="" />
-                <span>Vidéo</span>
-              </div>
-            </label>
-            <div className="item">
-              <img src={Map} alt="" />
-              <span>Géolocalisation</span>
-            </div>
-            <div className="item">
-              <img src={Friend} alt="" />
-              <span>Identifier un ou des amis</span>
-            </div>
-          </div>
-          <div className="right">
-            <button>Publier</button>
-          </div>
-        </div>
-        <Button variant="primary" onClick={handleShow}>
-          Launch demo modal
-        </Button>
 
-        <Modal show={show} onHide={handleClose} animation={false}>
-          <Modal.Header closeButton>
-            <Modal.Title>Modal heading</Modal.Title>
-          </Modal.Header>
-          <Modal.Body>Woohoo, you are reading this text in a modal!</Modal.Body>
-          <Modal.Footer>
-            <Button variant="secondary" onClick={handleClose}>
-              Close
-            </Button>
-            <Button variant="primary" onClick={handleClose}>
-              Save Changes
-            </Button>
-          </Modal.Footer>
-        </Modal>
+  function CreerPublication() {
+    return (
+      <div className="share">
+        <div className="container">
+          <div className="top">
+            <input placeholder={`Quoi de neuf /*{Sidiki Kaba}*/?*`} />
+          </div>
+          <hr />
+          <div className="bottom">
+            <div className="left">
+              <input type="file" id="file" style={{ display: "none" }} />
+              <label htmlFor="file">
+                <div className="item">
+                  <img src={Photo} alt="" />
+                  <span>Photo</span>
+                </div>
+              </label>
+              <input type="file" id="video" style={{ display: "none" }} />
+              <label htmlFor="file">
+                <div className="item">
+                  <img src={Video} alt="" />
+                  <span>Vidéo</span>
+                </div>
+              </label>
+              <div className="item">
+                <img src={Map} alt="" />
+                <span>Géolocalisation</span>
+              </div>
+              <div className="item">
+                <img src={Friend} alt="" />
+                <span>Identifier un ou des amis</span>
+              </div>
+            </div>
+            <div className="right">
+              <button>Publier</button>
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
+    );
+  }
+  return (
+    <>
+      <div variant="light" onClick={handleShow}>
+        <CreerPublication />
+      </div>
+
+      <Modal show={show} onHide={handleClose} animation={false} size="lg">
+        <Modal.Header closeButton>
+          <Modal.Title>Faire une publication</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          {" "}
+          <div className="share">
+            <div className="container">
+              <div className="top">
+                <textarea
+                  placeholder={`Quoi de neuf /*{Sidiki Kaba}*/?*`}
+                ></textarea>
+              </div>
+              <hr />
+              <div className="bottom">
+                <div className="left">
+                  <input type="file" id="file" style={{ display: "none" }} />
+                  <label htmlFor="file">
+                    <div className="item">
+                      <img src={Photo} alt="" />
+                      <span>Photo</span>
+                    </div>
+                  </label>
+                  <input type="file" id="video" style={{ display: "none" }} />
+                  <label htmlFor="file">
+                    <div className="item">
+                      <img src={Video} alt="" />
+                      <span>Vidéo</span>
+                    </div>
+                  </label>
+                  <div className="item">
+                    <img src={Map} alt="" />
+                    <span>Géolocalisation</span>
+                  </div>
+                  <div className="item">
+                    <img src={Friend} alt="" />
+                    <span>Identifier un ou des amis</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Annuler
+          </Button>
+          <Button
+            variant="primary"
+            onClick={() => {
+              console.log("message publié");
+              handleClose();
+            }}
+          >
+            Publier
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
   );
 };
 
