5cda2e58388665bd00629414d25a841b9d9c37cc
Sidiki Kaba <sidiki.kaba@disinstruments.fr>
2023-12-12 01:13:02 +0100

    menu de la page d'accueil

--- front_end/src/Components/menus/HomeMenu.js
+++ front_end/src/Components/menus/HomeMenu.js
@@ -4,9 +4,6 @@ import { Link } from "react-router-dom";
 import HomeIcon from "@mui/icons-material/Home";
 import LoginIcon from "@mui/icons-material/Login";
 import PersonAddIcon from "@mui/icons-material/PersonAdd";
-import Button from "react-bootstrap/Button";
-import OverlayTrigger from "react-bootstrap/OverlayTrigger";
-import Tooltip from "react-bootstrap/Tooltip";
 import "../../Styles/css/homeMenu.css";
 
 const MenuHorizontal = () => {
@@ -16,21 +13,23 @@ const MenuHorizontal = () => {
     <nav>
       <div className="topbarContainer">
         <div className="topbarLeft">
-          <Link to="/">
-            <div className="logo">
-              <HomeIcon fontSize="large" />
-              Viv<span>Exp@t</span>
-            </div>
+          <Link to="/" style={{ textDecoration: "none" }}>
+            <span className="logo">
+              Exp@t<span>media</span>
+            </span>
+            <span>
+              <h6 className="country">FRANCE</h6>
+            </span>
           </Link>
         </div>
         <div className="topbarRight">
           <div className="topbarIcons">
-            <Link to="/connexion" className="topbarIconItem">
+            <Link to="/page-de-connexion" className="topbarIconItem">
               <span>
                 <LoginIcon /> Se connecter
               </span>
             </Link>
-            <Link to="/inscription" className="topbarIconItem">
+            <Link to="/page-inscription-client" className="topbarIconItem">
               <span>
                 <PersonAddIcon /> S'inscrire
               </span>
@@ -63,13 +62,13 @@ const MenuHorizontal = () => {
             style={{ backgroundColor: "  rgb(94, 94, 97)" }}
           >
             <div className="topbarIcons">
-              <Link to="/connexion" className="topbarIconItem">
+              <Link to="/page-de-connexion" className="topbarIconItem">
                 <span>
                   <LoginIcon /> Se connecter
                 </span>
               </Link>
               <br />
-              <Link to="/inscription" className="topbarIconItem">
+              <Link to="/page-inscription-client" className="topbarIconItem">
                 <span>
                   <PersonAddIcon /> S'inscrire
                 </span>
